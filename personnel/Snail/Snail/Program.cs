7a236aa33a9aca64213a657aecb8c0ebcc0e1fd2
João Victor <123389747+JoaoVictor2023@users.noreply.github.com>
2024-08-30 10:16:30 +0200

    exSnail-v1.0

---
+++ personnel/Snail/Snail/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.SetCursorPosition(1,15);
+        Console.ForegroundColor = ConsoleColor.Blue;   
+        Console.WriteLine("_@_ö");
+        int vies = 1000;
+        for (int i = 0; i < 1000; i++)
+        {
+            Console.Clear();
+            Console.SetCursorPosition(i, 15);
+            Console.WriteLine("_@_ö");
+            vies -= 1;
+            Thread.Sleep(125);
+            if (vies == 0)
+            {
+                Console.Clear();
+                Console.SetCursorPosition(i, 15);
+                Console.WriteLine("____");
+            }
+        }
+        Console.ResetColor();
+    }
+}
