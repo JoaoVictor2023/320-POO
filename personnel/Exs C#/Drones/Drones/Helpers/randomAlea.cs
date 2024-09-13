3a91162fc74a991339085383de790157a27be871
João Victor <123389747+JoaoVictor2023@users.noreply.github.com>
2024-09-13 10:56:48 +0200

    create(drone): create folder drone with c# files

---
+++ personnel/Exs C#/Drones/Drones/Helpers/randomAlea.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Drones
+{
+    class randomAlea
+    {
+        public static Random alea = new Random();
+    }
+}
