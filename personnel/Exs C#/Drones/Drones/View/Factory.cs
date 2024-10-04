367fd90b26d7e2c86f61ce92ea5c06801656b2ca
João Victor <123389747+JoaoVictor2023@users.noreply.github.com>
2024-10-04 10:21:52 +0200

    feat(drone): Ajoutez une propriété `Id` à votre `Factory`, cela permettra de l'identifier.

--- personnel/Exs C#/Drones/Drones/View/Factory.cs
+++ personnel/Exs C#/Drones/Drones/View/Factory.cs
@@ -3,10 +3,14 @@
 namespace Drones
 {
     // Cette partie de la classe Drone définit comment on peut voir un drone
-
+    
     public partial class Factory : Building
     {
+        int idFactory;
+        public int IdFactory { get => idFactory; set => idFactory = value; }
+
         private Pen FactoryBrush = new Pen(new SolidBrush(Color.Purple), 2);
+
 
         // De manière graphique
         public void FactoryRender(BufferedGraphics drawingSpace)
